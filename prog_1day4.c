2ffdf22e8180ed8a8d5a35a3ab944dfb5f69fa12
mayank590036539 <malhan45mayank@gmail.com>
2026-08-13 18:15:40 +0530

    Add files via upload

---
+++ prog_1day4.c
@@ -0,0 +1,15 @@
+Q7: Write a program to swap two numbers without using a third variable.
+
+#include<stdio.h>
+int main(){
+int a,b;
+printf("a:");
+scanf("%d",&a);
+printf("b:");
+scanf("%d",&b);
+a=a+b;
+b=a-b;
+a=a-b;
+printf("a=%d,b=%d\n",a,b);
+return 0;
+}
